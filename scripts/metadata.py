0919c78de5923eed9adf4351b657d7e955626bf6
Martin <martin.soethe@uni-flensburg.de>
2018-11-22 16:16:30 +0100

    Restructure csv-files
    Allow for unique source for each entry.

--- scripts/metadata.py
+++ scripts/metadata.py
@@ -22,20 +22,21 @@ p.descriptor["contributors"] = [{
     "title": "'Simon Hilpert'",
     "email": "simon.hilpert@uni-flensburg.de",
     "role": "author"
+    }, {
+    "title": "'Martin Soethe'",
+    "email": "martin.soethe@uni-flensburg.de",
+    "role": "author"
     }
 ]
 
 p.descriptor["sources"] = [{
     "title": "DIW",
     "path": "https://www.diw.de/documents/publikationen/73/diw_01.c.424566.de/diw_datadoc_2013-068.pdf"
+    }, {
+    "title": "Brown",
+    "path": "10.1016/j.energy.2017.06.004"
     }
 ]
-
-# add to description to second field of the first resource
-p.descriptor['resources'][1]['schema']['fields'][3]['description'] = "Units in Euro/kW 2010"
-p.descriptor['resources'][1]['schema']['fields'][4]['description'] = "Lifetime of plant in years"
-p.descriptor['resources'][1]['schema']['fields'][5]['description'] = "Units in percentage per insatalled capacity kW/a"
-
 
 p.commit()
 
